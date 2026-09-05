13538faaa69bc518a2dd194c71f9a5da3c81910e
Helia.HK <heliahashemkhani7@gmail.com>
2026-09-05 20:41:13 +0330

    project detail's image

--- src/components/projectDetails/ProjectHighlights.tsx
+++ src/components/projectDetails/ProjectHighlights.tsx
@@ -7,17 +7,21 @@ type Props = {
 
 export default function ProjectHighlights({ project }: Props) {
   return (
-    <section className="flex flex-col gap-5  ">
+    <section className="flex flex-col gap-5">
       <h3 className="text-2xl md:text-3xl text-span font-bold">
         {project.highlights.title}
       </h3>
-      <div className="flex md:flex-row flex-col gap-4 justify-between w-full">
+
+      <div className="flex sm:flex-row flex-col  gap-4 justify-between w-full">
         {project.highlights.links.map((link) => (
-          <SpotlightCard>
+          <SpotlightCard
+            key={link}
+            className="bg-none w-full overflow-hidden rounded-2xl"
+          >
             <img
               src={link}
-              key={link}
-              className="object-cover border rounded-2xl md:max-w-xl"
+              className="w-full h-full object-cover border rounded-2xl"
+              alt=""
             />
           </SpotlightCard>
         ))}
